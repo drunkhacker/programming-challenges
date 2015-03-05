7541c5f2e3e9e9a341da5435615d43424ab956ea
Jaehwa Han <drh@wafflestudio.com>
2015-03-05 18:00:18 +0900

    110702 solved

---
+++ 110702.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <math.h>
+
+#define MAX_N 65000
+
+int is_prime[MAX_N];
+
+int mod_exp(unsigned int a, unsigned int n)
+{
+    int s = 1;
+    int m = n;
+    //calc a^n mod n
+    while (n > 0) {
+        if (n % 2) {
+            s = s*a % m;
+            n -= 1;
+        }
+
+        a = a*a % m;
+        n /= 2;
+    }
+
+    return s;
+}
+
+int carmichael(int n)
+{
+    int i, k;
+
+    if (is_prime[n])
+        return 0;
+
+    for (i = 2; i < n; i++) {
+        if (mod_exp(i, n) != i) {
+            /* debug */
+            /* printf("%d^%d (mod %d) = %d\n", i, n, n, mod_exp(i, n)); */
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+void pre_calc_prime()
+{
+    int n, i;
+
+    is_prime[1] = 0;
+    is_prime[2] = 1;
+
+    for (n = 3; n < MAX_N; n++)  {
+        is_prime[n] = 1;
+        for (i = 2; i < sqrt(n)+1; i++) {
+            if (n % i == 0) {
+                is_prime[n] = 0;
+                break;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+
+    /* mod_exp test */
+    /* printf("1924^3800 (mod 3800) = %d\n", mod_exp(1924, 3800)); */
+
+    pre_calc_prime();
+
+    /* printf("9 prime? %d\n", is_prime[9]); */
+
+    while (1) {
+        scanf("%d", &n);
+
+        if (n == 0)
+            break;
+
+        if (carmichael(n))
+            printf("The number %d is a Carmichael number.\n", n);
+        else
+            printf("%d is normal.\n", n);
+    }
+
+    return 0;
+}
