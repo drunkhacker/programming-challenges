31ed2db979630d3eaf35a664d2f91f374f49cc2b
Jaehwa Han <drh@wafflestudio.com>
2015-03-16 22:05:32 +0900

    110801 solved

--- 110801.c
+++ 110801.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-int white_board[8][8]; //board[i][j]>0 <=> position(i,j) is placed or not avail.
-int black_board[8][8];
+#define MAX_N 8
+#define abs(a) ((a)<0?(-(a)):(a))
 
-long white_count[64];
-long black_count[64];
+int board_size;
+int bishop_count;
+long answer_count;
+
+long white_count[MAX_N+1][65];
+long black_count[MAX_N+1][65];
 
 typedef struct cell_s cell_t;
 struct cell_s {
@@ -13,140 +17,114 @@ struct cell_s {
     int col;
 };
 
-cell_t empty_cells[64];
-
-
-void place2(int row, int col, int board[8][8], int board_size, int d)
+void find_candidates(cell_t a[], int k, cell_t cand[], int *n_cand, int board)
 {
-    int dr, dc;
-    int r, c;
+    int r, c, i;
+    //a[k]에 들어갈 수 있는 애들을 탐색한다
+    //현재까지 a[k-1]까지의 답이 construct돼있음
+    //
+    //a[k-1]의 컬럼이 현재까지의 답 중에 가장 큰 컬럼
+    *n_cand = 0;
 
-    board[row][col] += d;
-    /* printf("place2\n"); */
-    /* printf("(r,c) = (%d,%d)\n", row, col); */
-
-    for (dr = -1; dr <= 1; dr += 2) {
-        for (dc = -1; dc <= 1; dc += 2) {
-            for (r = row, c = col; 
-                 r >= 0 && r < board_size && c >= 0 && c < board_size;
-                 r += dr, c += dc) {
-                if (r == row && c == col)
-                    continue;
-
-                /* printf("(r,c) = (%d,%d)\n", r, c); */
-                board[r][c] += d;
-            }
-        }
-    }
-}
-
-void place(int row, int col, int board[8][8], int board_size)
-{
-    place2(row, col, board, board_size, 1);
-}
-
-void displace(int row, int col, int board[8][8], int board_size)
-{
-    place2(row, col, board, board_size, -1);
-}
-
-int get_empty_cell(int board[8][8], int board_size, cell_t *result)
-{
-    int r, c;
-    int n = 0;
     for (r = 0; r < board_size; r++) {
-        for (c = 0; c < board_size; c++) {
-            if (board[r][c] == 0)
-                result[n++] = (cell_t){r,c};
-        }
-    }
-
-    return n;
-}
-
-long backtrack(int i, int k, int board[8][8], int board_size)
-{
-    long c = 0;
-    int empty_cnt;
-    int j;
-    cell_t cell;
-
-    if (k == 0) 
-        return 0;
-
-    if (i == k)
-        return 1;
-
-    empty_cnt = get_empty_cell(board, board_size, empty_cells);
-
-    if (empty_cnt < k - i)
-        return 0;
-
-    for (j = 0; j < empty_cnt; j++) {
-        cell = empty_cells[j];
-        place(cell.row, cell.col, board, board_size);
-        c += backtrack(i + 1, k, board, board_size);
-        displace(cell.row, cell.col, board, board_size);
-        if (empty_cnt != get_empty_cell(board, board_size, empty_cells)) {
-            printf("error! empty_cnt=%d, empty_cnt2=%d\n", empty_cnt,
-                    get_empty_cell(board, board_size, empty_cells));
-            exit(1);
-
-        }
-    }
-
-    return c;
-}
-
-long process(int n, int k)
-{
-    int i, j, m;
-    long c = 0;
-    long c1, c2;
-
-    for (m = 0; m <= k; m++) {
-        /* memset(white_count, 0, sizeof(white_count)); */
-        /* memset(black_count, 0, sizeof(black_count)); */
-
-        memset(white_board, 0, sizeof(white_board));
-        memset(black_board, 0, sizeof(black_board));
-
-        for (i = 0; i < 8; i++) {
-            for (j = 0; j < 8; j++) {
-                if (i % 2 == j % 2) {
-                    white_board[i][j] = 1;
-                    /* printf("* "); */
-                    /* printf("(r,c)=(%d,%d)\n", i, j); */
-                } else {
-                    black_board[i][j] = 1;
-                    /* printf("  "); */
-                }
+        for (c = (k == 0 ? 0 : a[k-1].col); c < board_size; c++) {
+            if (board) { //black board
+                if (r % 2 != c % 2)
+                    continue;
+            } else { //white board
+                if (r % 2 == c % 2)
+                    continue;
             }
-                /* printf("\n"); */
+
+            for (i = 0; i < k; i++) {
+                if (a[i].col == c && a[i].row > r) {
+                    break;
+                }
+                if (abs(a[i].row - r) == abs(a[i].col - c))
+                    break;
+            }
+
+            if (i == k) {
+                cand[*n_cand] = (cell_t){r, c};
+                *n_cand = *n_cand + 1;
+            }
         }
+    }
+}
 
-        c1 = backtrack(0, m, white_board, n);
-        c2 = backtrack(0, k-m, black_board, n);
+void backtrack(cell_t a[], int k, int n, int board)
+{
+    int i;
+    int c_cnt;
+    cell_t c[64];
 
-        printf("white_count(%d, %d) = %ld, black_count(%d, %d) = %ld\n",
-                n, m, c1, n, k-m, c2);
-
-        c += c1 + c2;
+    if (n == 0) {
+        answer_count = 1;
+        return;
     }
 
-    return c;
+    if (k == n) {
+        /* for (i = 0; i < k; i++) {
+            printf("(%d,%d) ", a[i].row, a[i].col);
+        }
+        printf("\n"); */
+        answer_count++;
+    } else {
+        find_candidates(a, k, c, &c_cnt, board);
+        for (i = 0; i < c_cnt; i++) {
+            a[k] = c[i];
+            backtrack(a, k+1, n, board);
+        }
+    }
+}
+
+void process()
+{
+    int i, j;
+    long c = 0;
+    long c1 = 0;
+
+    cell_t a1[64];
+    cell_t a2[64];
+    for (i = 0; i <= bishop_count; i++) {
+        if (white_count[board_size][i] < 0) {
+            answer_count = 0;
+            backtrack(a1, 0, i, 0);
+            white_count[board_size][i] = answer_count;
+        }
+        c1 = white_count[board_size][i];
+
+        j = bishop_count - i;
+        if (black_count[board_size][j] < 0) {
+            answer_count = 0;
+            backtrack(a2, 0, j, 1);
+            black_count[board_size][j] = answer_count;
+        }
+        c1 *= black_count[board_size][j];
+
+        c += c1;
+    }
+
+    printf("%ld\n", c);
 }
 
 int main()
 {
-    int n, k;
+    int i, n;
+
+    for (n = 1; n <= 8; n++) {
+        for (i = 0; i <= 64; i++) {
+            white_count[n][i] = -1;
+            black_count[n][i] = -1;
+        }
+    }
 
     while (1) {
-        scanf("%d%d", &n, &k);
-        if (n == 0 && k == 0)
+        scanf("%d%d", &board_size, &bishop_count);
+        if (board_size == 0 && bishop_count == 0)
             break;
 
-        printf("%ld\n", process(n, k));
+        process();
     }
 
     return 0;
